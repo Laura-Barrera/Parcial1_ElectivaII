0c8ae70953b854f4f8c2b3651218ce67e99e52e1
Laura-Barrera <laura.barrera14@uptc.edu.co>
2023-07-06 20:15:01 -0500

    Avance 1

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ router.get('/',(req,res)=> res.render('index',{'title':'Melissa Barrera'}))
 
 router.get('/visualizacion',(req,res)=> res.render('visualizar',{'title':'Melissa Barrera','peliculas': miMapa}))
 
-router.get('/eliminar',(req,res)=> res.render('eliminar',{'title':'Melissa Barrera'}))
+router.get('/eliminar',(req,res)=> res.render('eliminar',{'title':'Melissa Barrera','peliculas': miMapa}))
 
 module.exports = router
